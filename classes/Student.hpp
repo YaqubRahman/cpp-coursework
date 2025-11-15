c0e2860d71d93cdfd65d25692eaf2dbdccaecb33
YaqubRahman <YaqubRahman999@gmail.com>
2025-11-15 16:58:56 +0000

    first commit

---
+++ classes/Student.hpp
@@ -0,0 +1,15 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include <vector>
+
+class Student {
+    std:: string id;
+    std::vector<double> marks;
+    double term_average;
+    double overal_average;
+
+};
+
+#endif
