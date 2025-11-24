285cdd3545bf3756e1d4e4089cca876ddc364cd2
YaqubRahman <YaqubRahman999@gmail.com>
2025-11-24 18:20:28 +0000

    feat:working in clion

--- main.cpp
+++ main.cpp
@@ -9,10 +9,40 @@
 
 using namespace std;
 
+// Vector holding all the student objects
 vector<Student> students;
+
+// Map holding all the module codes and module names
+map<string, string> module_names;
+
+
+// int add_modules_record() {
+//     ifstream in("data/modules.txt");
+//     string line;
+//     string module_code;
+//     string module_name;
+//
+//     while (getline(in, line)) {
+//         istringstream line_s(line);
+//
+//         if(!(line_s >> module_code)){
+//             continue;
+//         }
+//
+//         if(line_s >> ws && getline(line_s, module_name)){
+//             module_names[module_code] = module_name;
+//         }
+//     }
+//     return 0;
+// }
 
 int add_student_record() {
     ifstream in("data/requests.txt");
+    if (!in) {
+        cerr << "Unable to open data/requests.txt" << endl;
+    }
+
+
     string line;
     string student_id;
     double term_code;
@@ -32,6 +62,14 @@ int add_student_record() {
     }
     return 0;
 }
+
+// int add_terms_record() {
+//     ifstream in("data/grades.txt");
+//     ifstream in("data/modules.txt");
+//
+//     for(int i = 0; i < students.size(); i++){
+//     }
+// }
 
 int main() {
     add_student_record();
